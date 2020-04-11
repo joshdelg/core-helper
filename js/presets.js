82494565b5ef47478dea4c99e4e0dbb635f3088b
pixelpython <alexanderbarran@gmail.com>
2020-04-10 17:59:45 -0700

    Add files via upload

---
+++ js/presets.js
@@ -0,0 +1,36 @@
+var presets = [
+	{
+		"name":"Dirty Dozen",
+		"steps": [
+			{"name":"Bicycle Kicks", "time":60},
+			{"name":"Push Up Side Plank", "reps":20},
+			{"name":"Back Bridge", "reps":15},
+			{"name":"Ab Scissors", "reps":30},
+			{"name":"Plank", "time":60},
+			{"name":"Fire Hydrant", "reps":"15 each side"},
+			{"name":"Side Plank Left", "time":30},
+			{"name":"Side Plank Right", "time":30},
+			{"name":"Donkey Extension", "reps":"15 each side"},
+			{"name":"V-Ups", "reps":15},
+			{"name":"Prone Knee Thrust", "reps":"5 each side"},
+			{"name":"Standing Glute-Iso's", "reps":"5 front, side, rear; each side"},
+			{"name":"Push Up", "reps":55}
+		]
+	},
+	{
+		"name":"Alec's Fun Workout",
+		"steps": [
+			{"name":"Russian Twist", "time":120, "special":30},
+			{"name":"Bicycle Kicks", "time":120},
+			{"name":"Dumbbell Squats", "reps":"4x25 reps"},
+			{"name":"Plank", "time":120},
+			{"name":"Side Plank Left", "time":60, "special":15},
+			{"name":"Side Plank Right", "time":60, "special":15},
+			{"name":"Standing Glute-Iso's", "reps":"5 front, side, rear; each side"},
+			{"name":"Ab Scissors", "reps":100},
+			{"name":"Pushups", "reps":55},
+			{"name":"Situps", "reps":"10x10 reps"},
+			{"name":"Leg Push Downs", "reps":"2x25 reps"}
+		]
+	}
+]
